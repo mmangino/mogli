91b4fa16084cad6c91be2d1a07c3cecbb3866831
Kimball Bighorse <kbighorse@yahoo.com>
2010-05-04 12:56:09 -0700

    Adding properties to page.rb

--- lib/mogli/page.rb
+++ lib/mogli/page.rb
@@ -9,6 +9,15 @@ module Mogli
     hash_populating_accessor :posts, "Post"
     hash_populating_accessor :events, "Event"
     hash_populating_accessor :videos, "Video"
+    hash_populating_accessor :founded, "Founded"
+    hash_populating_accessor :products, "Products"
+    hash_populating_accessor :username, "Username"
+    hash_populating_accessor :mission, "Mission"
+    hash_populating_accessor :company_overview, "Company Overview"
+    hash_populating_accessor :fan_count, "Fan Count"
+    hash_populating_accessor :has_added_app, "Has Added App"
+    hash_populating_accessor :link, "Link"
+    hash_populating_accessor :picture, "Picture"
     
     def self.recognize?(hash)
       hash.has_key?("category")
