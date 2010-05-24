74d8390877b520c0837b2b1205738c2e69865cff
Kimball Bighorse <kbighorse@yahoo.com>
2010-05-24 14:32:11 -0700

    adding creation properties for an event

--- lib/mogli/page.rb
+++ lib/mogli/page.rb
@@ -1,15 +1,8 @@
 module Mogli
   class Page < Profile
         
-    define_properties :id, :name, :category, :founded, :products, :username, 
-      :mission, :company_overview, :fan_count, :has_added_app, :link, :picture
+    define_properties :id, :name, :category
     
-    hash_populating_accessor :albums, "Album"
-    hash_populating_accessor :photos, "Photo"
-    hash_populating_accessor :feed, "Post"
-    hash_populating_accessor :posts, "Post"
-    hash_populating_accessor :events, "Event"
-    hash_populating_accessor :videos, "Video"
     
     def self.recognize?(hash)
       hash.has_key?("category")
