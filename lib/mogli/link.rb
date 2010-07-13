8cf17681bea1b730b0bd3ec4c0b42f72a46d8c4f
Nic Cavigliano <ncavig@gmail.com>
2010-07-13 14:46:58 -0700

    filling out Link with properties and comments/from accessors

--- lib/mogli/link.rb
+++ lib/mogli/link.rb
@@ -1,6 +1,9 @@
 module Mogli
   class Link < Model
-    define_properties :link, :message
+    define_properties :link, :message, :id, :name, :page, :created_time, :icon, :picture, :description
     creation_properties :link, :message
+
+    hash_populating_accessor :comments, "Comment"
+    hash_populating_accessor :from, "User"
   end
 end
