0d513a5c678e195a7f7780cd802b577e78a728b1
Alex Sherstinsky <shers@thinkpad.crib>
2010-07-21 09:09:50 -0700

    Added the access_token property to page.rb and over-wrote the initializer method in order to create a special Client for the page.

--- lib/mogli/page.rb
+++ lib/mogli/page.rb
@@ -1,7 +1,7 @@
 module Mogli
   class Page < Profile
         
-    define_properties :id, :name, :category, :username
+    define_properties :id, :name, :category, :username, :access_token
     
     # General
     define_properties :fan_count, :link, :picture, :has_added_app
@@ -12,6 +12,21 @@ module Mogli
     # Musicians
     define_properties :record_label, :hometown, :band_members, :genre
     
+	# When "/me/accounts" returns Page with 'access_token', this Page can be
+	# impersonated by an application.  The expiration value must be taken from
+	# the caller object (e.g., User) so as to uphold the original permissions.
+	def initialize(hash = {}, client = nil)
+		super(hash, client)
+		unless(access_token.blank?)
+			if(client.blank?)
+				self.client = Mogli::Client.new(access_token)
+			else
+				self.client =
+				Mogli::Client.new(access_token, client.expiration)
+			end
+		end
+	end
+
     def self.recognize?(hash)
       hash.has_key?("category")
     end
