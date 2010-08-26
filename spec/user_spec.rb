cda39459bcd735849bfc882d298147eeaef66097
Damian Nurzynski <dnurzynski@gmail.com>
2010-08-26 21:07:42 +0200

    Added finding by many ids

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,29 +1,28 @@
 require "spec_helper"
 describe Mogli::User do
-  
+
   let :user_1 do
     user_1 = Mogli::User.new(:id=>1)
     user_1.client = mock_client
     user_1
   end
-  
+
   let :mock_client do
     mock("client")
   end
-  
 
-  
+
   it "won't recognize pages" do
     Mogli::User.recognize?("id"=>1,"name"=>"bob","category"=>"fdhsjkfsd")
   end
-  
+
   it "will recognize itself" do
-    Mogli::User.recognize?("id"=>1,"name"=>"bob")    
+    Mogli::User.recognize?("id"=>1,"name"=>"bob")
   end
-  
-  
+
+
   describe "Finding" do
-    
+
     it "finds optional fields for a user" do
       Mogli::Client.should_receive(:get).with("https://graph.facebook.com/1",
         :query=>{:fields => [:birthday, :gender]}).and_return({:id=>1, :birthday=>'09/15', :gender => 'male'})
@@ -31,7 +30,7 @@ describe Mogli::User do
       user.birthday.should == '09/15'
       user.gender.should == 'male'
     end
-  
+
     it "finds a user's friends with optional fields" do
       mock_client.should_receive(:get_and_map).with(
         "1/friends", "User", {:fields => [:birthday, :gender]}).and_return(
@@ -40,5 +39,5 @@ describe Mogli::User do
       friends.size.should == 1
     end
   end
-  
+
 end
