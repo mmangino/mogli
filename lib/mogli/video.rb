1c9be5b79e97f65d76783d2d93fbed43d9be9062
Damian Nurzynski <dnurzynski@gmail.com>
2010-09-07 21:49:33 +0200

    Add missing properties for link, status & video

--- lib/mogli/video.rb
+++ lib/mogli/video.rb
@@ -1,11 +1,12 @@
 module Mogli
   class Video < Model
-    
-    define_properties :id, :message, :name, :description, :length, :created_time, :updated_time, :icon, :picture, :embed_html
-    
+
+    define_properties :id, :message, :name, :description, :length, :created_time,
+      :updated_time, :icon, :picture, :embed_html, :caption, :link, :source, :likes
+
     hash_populating_accessor :from, "User", "Page"
-    
+
     has_association :comments, "Comment"
-    
+
   end
 end
