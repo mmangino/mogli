ef2a3ba8c7d17521c0eb11992802938318187a61
Damian Nurzynski <dnurzynski@gmail.com>
2010-09-07 21:24:13 +0200

    Added search feature

--- lib/mogli/education.rb
+++ lib/mogli/education.rb
@@ -1,8 +1,8 @@
 module Mogli
   class Education < Model
-    
+
     define_properties :start_date, :end_date, :degree
-    
+
     hash_populating_accessor :school, "Page"
     hash_populating_accessor :year, "Page"
     hash_populating_accessor :concentration, "Page"
--- lib/mogli/page.rb
+++ lib/mogli/page.rb
@@ -1,34 +1,35 @@
 module Mogli
   class Page < Profile
-        
+    set_search_type
+
     define_properties :id, :name, :category, :username, :access_token
-    
+
     # General
     define_properties :fan_count, :link, :picture, :has_added_app
 
     # Retail
     define_properties :founded, :products, :mission, :company_overview
-    
+
     # Musicians
     define_properties :record_label, :hometown, :band_members, :genre
-    
-    # As a like 
+
+    # As a like
     define_properties :created_time
-    
-  	def client_for_page
-  	  if access_token.blank?
-  	    raise MissingAccessToken.new("You can only get a client for this page if an access_token has been provided. i.e. via /me/accounts")
+
+    def client_for_page
+      if access_token.blank?
+        raise MissingAccessToken.new("You can only get a client for this page if an access_token has been provided. i.e. via /me/accounts")
       end
       Client.new(access_token)
     end
-  
+
     def self.recognize?(hash)
       hash.has_key?("category")
     end
 
     class MissingAccessToken < Exception
     end
-    
+
   end
-  
+
 end
