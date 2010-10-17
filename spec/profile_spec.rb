bdfeea00370242c0c85624f7113f9432f883e877
Bradley Schaefer <bradley@anythingsocial.com>
2010-10-17 14:25:55 -0700

    Added methods for getting square/large/small image urls for Profiles

--- spec/profile_spec.rb
+++ spec/profile_spec.rb
@@ -5,4 +5,12 @@ describe Mogli::Profile do
     profile.id="1"
     profile.image_url.should == "https://graph.facebook.com/1/picture"
   end
+  
+  it "has image_url methods for small/large/square" do
+    profile = Mogli::Profile.new
+    profile.id="1"
+    profile.square_image_url.should == "https://graph.facebook.com/1/picture?type=square"
+    profile.large_image_url.should == "https://graph.facebook.com/1/picture?type=large"
+    profile.small_image_url.should == "https://graph.facebook.com/1/picture?type=small"
+  end
 end
