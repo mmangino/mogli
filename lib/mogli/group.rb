a7e9b535802ed9fb1708030df825b3e03609612d
yuri0 <yuri@ualberta.ca>
2010-10-30 22:02:50 -0600

    Adding properties to group.rb

--- lib/mogli/group.rb
+++ lib/mogli/group.rb
@@ -1,7 +1,7 @@
 module Mogli
   class Group < Model
     set_search_type
-    define_properties :id, :name, :description, :link, :privacy, :updated_time
+    define_properties :id, :name, :description, :link, :privacy, :updated_time, :version, :administrator
 
     hash_populating_accessor :owner, "User", "Page"
     hash_populating_accessor :venue, "Address"
