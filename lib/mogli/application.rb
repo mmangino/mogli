beae79b5fca2da6b24af7d1505dcfee27a464d9c
sarcos <soxos.inc@gmail.com>
2010-11-03 21:03:15 -0400

    added Application Insights basic objects

---
+++ lib/mogli/application.rb
@@ -0,0 +1,13 @@
+module Mogli
+  class Application < Model
+
+    define_properties :id, :name, :description, :category, :subcategory, :link
+
+    has_association :feed, "Post"
+    has_association :posts, "Post"
+    has_association :albums, "Album"
+    has_association :insights, "Insight"
+
+
+  end
+end
