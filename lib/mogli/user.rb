3541eadd956bd5323bcec149f59e7fb834f98adf
Brian Percival <percival@peredur.local>
2010-11-05 15:35:13 -0700

    Check for facebook extended permissions on facebook user

--- lib/mogli/user.rb
+++ lib/mogli/user.rb
@@ -26,5 +26,32 @@ module Mogli
     has_association :likes, "Page"
     has_association :home, "Post"
     has_association :accounts, "Page"
+
+    # allows querying facebook to see if the user has granted the specified
+    # permissions. the permissions arg should be an array of strings or
+    # symbols matching the facebook permissions exactly:
+    # http://developers.facebook.com/docs/authentication/permissions
+    #
+    # returns a hash from permissions to booleans indicating presence or
+    # absence of permission
+    def check_permissions(permissions)
+      fql = "SELECT #{permissions.map(&:to_s).join(',')} " +
+            "FROM permissions " +
+            "WHERE uid = #{self.id}"
+
+      result = client.fql_query(fql).parsed_response.first
+
+      result.each_pair do |perm, val|
+        result[perm] = (val == 1)
+      end
+    end
+
+    # check if the facebook user has a specific permission
+    # the permission arg should be a string or symbol matching the facebook
+    # permissions exactly:
+    # http://developers.facebook.com/docs/authentication/permissions
+    def has_permission?(permission)
+      check_permissions([permission])[permission]
+    end
   end
 end
