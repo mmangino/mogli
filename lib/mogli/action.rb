7d2115830c7c4726c65a8d5ebac2afb51e427620
Brian Percival <percival@peredur.local>
2010-11-05 15:50:57 -0700

    adding options arg for to_json, which is passed in by rails encoding logic when Array is to_json()ed

--- lib/mogli/action.rb
+++ lib/mogli/action.rb
@@ -2,7 +2,10 @@ module Mogli
   class Action < Model
     define_properties :name, :link
 
-    def to_json
+    # simple implementation of to_json, ignoring options like :only, :except,
+    # :include, :methods because this is primarily intended for being submitted
+    # to facebook
+    def to_json(options = nil)
       "{\"name\":\"#{name}\",\"link\":\"#{link}\"}"
     end
   end
