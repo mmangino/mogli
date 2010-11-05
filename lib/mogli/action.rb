e4bc42af2f69ea41973e4b9f70fdc5e431839d47
Brian Percival <percival@peredur.local>
2010-11-05 15:11:25 -0700

    added ability to include associated models when posting to facebook (e.g. adding :actions to a Post)

--- lib/mogli/action.rb
+++ lib/mogli/action.rb
@@ -1,6 +1,12 @@
 module Mogli
   class Action < Model
     define_properties :name, :link
-    
+
+    # simple implementation of to_json, ignoring options like :only, :except,
+    # :include, :methods because this is primarily intended for being submitted
+    # to facebook
+    def to_json(options = nil)
+      "{\"name\":\"#{name}\",\"link\":\"#{link}\"}"
+    end
   end
 end
