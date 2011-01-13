02c9e74a2af31c611a761c13dd8c5d6372d30a17
Keith Morrison <keithm@infused.org>
2011-01-13 10:21:26 -0800

    Fix app_client specs

--- spec/app_client_spec.rb
+++ spec/app_client_spec.rb
@@ -8,33 +8,32 @@ describe Mogli::AppClient do
     end
     
     it "should allow you to subscribe with callback url, fields and verify token" do
-      Mogli::Client.should_receive(:post).with("http://graph.facebook.com/123/subscriptions",an_instance_of(Hash))
+      Mogli::AppClient.should_receive(:post).with("https://graph.facebook.com/123/subscriptions",an_instance_of(Hash))
       client.subscribe_to_model(Mogli::User,:callback_url=>"http://localhost:3000",:fields=>["username"])
     end
     
     it "should pass the type as the object field" do
-      Mogli::Client.should_receive(:post).with("http://graph.facebook.com/123/subscriptions",{:body=>hash_including(:object=>"user")})
+      Mogli::AppClient.should_receive(:post).with("https://graph.facebook.com/123/subscriptions",{:body=>hash_including(:object=>"user")})
       client.subscribe_to_model(Mogli::User,:callback_url=>"http://localhost:3000",:fields=>["username"])
     end
     
     it "sends the list of fields comma separated" do
-      Mogli::Client.should_receive(:post).with("http://graph.facebook.com/123/subscriptions",{:body=>hash_including(:fields=>"username,friends")})
+      Mogli::AppClient.should_receive(:post).with("https://graph.facebook.com/123/subscriptions",{:body=>hash_including(:fields=>"username,friends")})
       client.subscribe_to_model(Mogli::User,:callback_url=>"http://localhost:3000",:fields=>["username,friends"])
     end
     it "sends the callback url" do
-      Mogli::Client.should_receive(:post).with("http://graph.facebook.com/123/subscriptions",{:body=>hash_including(:callback_url=>"http://localhost:3000")})
+      Mogli::AppClient.should_receive(:post).with("https://graph.facebook.com/123/subscriptions",{:body=>hash_including(:callback_url=>"http://localhost:3000")})
       client.subscribe_to_model(Mogli::User,:callback_url=>"http://localhost:3000",:fields=>["username"])
     end
     
     it "should send the verify token if provided" do
-      Mogli::Client.should_receive(:post).with("http://graph.facebook.com/123/subscriptions",{:body=>hash_including(:verify_token=>"TOKEN")})
+      Mogli::AppClient.should_receive(:post).with("https://graph.facebook.com/123/subscriptions",{:body=>hash_including(:verify_token=>"TOKEN")})
       client.subscribe_to_model(Mogli::User,:callback_url=>"http://localhost:3000",:fields=>["username"],:verify_token=>"TOKEN")
-      
     end
     
     it "Lists the existing subscriptions" do
-     a.should_receive(:get_and_map_url).with("http://graph.facebook.com/123/subscriptions","Subscription")
-     a.subscriptions
+     client.should_receive(:get_and_map_url).with("https://graph.facebook.com/123/subscriptions","Subscription")
+     client.subscriptions
     end
   end
 
