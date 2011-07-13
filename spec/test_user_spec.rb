9c27b11bf696de11fd5ac4aa01c144d3c4681b66
Travis Vachon <travis@copious.com>
2011-07-13 11:21:15 -0400

    Rename creation query parameter, document creation params
    Doco ripped shamelessly from:
    
    http://developers.facebook.com/docs/test_users/
    
    Also, drop query parameter on TestUser#all, as there are no documented options.

--- spec/test_user_spec.rb
+++ spec/test_user_spec.rb
@@ -7,8 +7,9 @@ describe Mogli::User do
   describe "#create" do
     it "POSTs to the test user creation url" do
       Mogli::Client.should_receive(:post).with("https://graph.facebook.com/#{app_id}/accounts/test-users",
-        :body => {:access_token => access_token}).and_return({:id=>1, :login_url => 'http://example.com/hamsocks' })
-      user = Mogli::TestUser.create({}, Mogli::AppClient.new(access_token, app_id))
+        :body => {:access_token => access_token, :installed => false, :name => 'The Sock Hammer'}).
+        and_return({:id=>1, :login_url => 'http://example.com/hamsocks' })
+      user = Mogli::TestUser.create({:installed => false, :name => 'The Sock Hammer'}, Mogli::AppClient.new(access_token, app_id))
       user.login_url.should == 'http://example.com/hamsocks'
     end
   end
@@ -17,7 +18,7 @@ describe Mogli::User do
     it "GETs the test user url" do
       Mogli::Client.should_receive(:get).with("https://graph.facebook.com/#{app_id}/accounts/test-users",
         :query => {:access_token => access_token}).and_return([{:id=>1, :login_url => 'http://example.com/hamsocks'}])
-      users = Mogli::TestUser.all({}, Mogli::AppClient.new(access_token, app_id))
+      users = Mogli::TestUser.all(Mogli::AppClient.new(access_token, app_id))
       users.first.login_url.should == 'http://example.com/hamsocks'
     end
   end
