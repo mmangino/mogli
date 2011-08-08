e21c6f23ccd470d2aca006627dc2fac071e138c5
peer2 <charles@peer2.com>
2011-08-08 14:26:09 -0700

    adding from

--- lib/mogli/work.rb
+++ lib/mogli/work.rb
@@ -1,7 +1,7 @@
 module Mogli
   class Work < Model
     
-    define_properties :start_date, :end_date, :description, :with
+    define_properties :start_date, :end_date, :description, :with, :from
     
     hash_populating_accessor :projects, "Page"
     hash_populating_accessor :employer, "Page"
