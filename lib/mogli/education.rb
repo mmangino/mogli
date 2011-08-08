bcad11284e01592c9d2d80e7ebb4f134350b7cdb
peer2 <charles@peer2.com>
2011-08-08 14:10:21 -0700

    adding classes

--- lib/mogli/education.rb
+++ lib/mogli/education.rb
@@ -3,6 +3,7 @@ module Mogli
 
     define_properties :start_date, :end_date, :degree
 
+    hash_populating_accessor :classes, "Page"
     hash_populating_accessor :school, "Page"
     hash_populating_accessor :year, "Page"
     hash_populating_accessor :concentration, "Page"
