bca05a98a67e5ef6c234fbf31b76d5522d29de47
Kristján Pétursson <kristjan@causes.com>
2011-09-21 13:40:15 -0700

    Stop relying on `blank?`
    `blank?` was available on Strings, Hashes, etc. because the crack gem added it,
    but HTTParty doesn't depend on crack as of v0.8.0. Checking `nil?` and `empty?`
    is more verbose and a little irritating, but will be more dependable.

--- lib/mogli/fetching_array.rb
+++ lib/mogli/fetching_array.rb
@@ -1,17 +1,17 @@
 module Mogli
   class FetchingArray < Array
     attr_accessor :next_url, :previous_url, :client, :classes
-    
+
     def fetch_next
-      return [] if next_url.blank?
+      return [] if next_url.nil? || next_url.empty?
       additions = client.get_and_map_url(next_url,classes)
       self.next_url = additions.next_url
       self.concat(additions)
       additions
     end
-    
+
     def fetch_previous
-      return [] if previous_url.blank?
+      return [] if previous_url.nil? || previous_url.empty?
       additions = client.get_and_map_url(previous_url,classes)
       self.previous_url = additions.previous_url
       self.unshift(*additions)
