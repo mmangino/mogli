a7768562892cf2255d51c23adeb76c431817ce33
Giovanni Intini <giovanni@mikamai.com>
2011-12-19 18:18:40 +0100

    Revert "adding missing fields to comments"
    This reverts commit 12cf2004957ca87703a6487544aeeebedddcb3e4.

--- lib/mogli/comment.rb
+++ lib/mogli/comment.rb
@@ -1,9 +1,9 @@
 module Mogli
   class Comment < Model
-
-    define_properties :id, :message, :created_time, :count, :likes, :can_remove, :message_tags
+    
+    define_properties :id, :message, :created_time, :count, :likes
     creation_properties :message
     hash_populating_accessor :from, "User","Page"
-
+    
   end
 end
