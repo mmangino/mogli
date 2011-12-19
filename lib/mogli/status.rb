f7931c412069f78f74dbe94cffba4e87dbe02aa3
Giovanni Intini <giovanni@mikamai.com>
2011-12-19 16:15:25 +0100

    adding comments and likes to statuses

--- lib/mogli/status.rb
+++ lib/mogli/status.rb
@@ -1,7 +1,9 @@
 module Mogli
   class Status < Model
-    define_properties :id, :message, :updated_time, :created_time
+    define_properties :id, :message, :updated_time, :created_time, :likes
 
     hash_populating_accessor :from, "User", "Page"
+
+    has_association :comments, "Comment"
   end
 end
