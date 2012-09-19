919ccecdc935342ee73b8a278ee6decd9f79d8db
Tim Olshansky <tim.olshansky@gmail.com>
2012-09-19 14:55:41 -0700

    Added missing properties to Album and Photo

--- lib/mogli/album.rb
+++ lib/mogli/album.rb
@@ -1,12 +1,12 @@
 module Mogli
   class Album < Model
-    
-    define_properties :id, :name, :description, :location, :cover_photo, :privacy, :link, :count, :created_time, :updated_time
+    define_properties :id, :name, :description, :location, :cover_photo, :privacy, :link, :count, :created_time, :updated_time,
+      :likes, :can_upload
     creation_properties :name, :description
-    
+
     hash_populating_accessor :from, "User","Page"
     has_association :photos, "Photo"
     has_association :comments, "Comment"
-    
+
   end
 end
