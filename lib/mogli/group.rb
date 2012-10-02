017bbd77f6c96bd39b0ccb9ca2f46235be2c1834
Tim Olshansky <tim.olshansky@gmail.com>
2012-10-02 14:02:52 -0700

    Added missing properties to Event and Group models

--- lib/mogli/group.rb
+++ lib/mogli/group.rb
@@ -1,7 +1,8 @@
 module Mogli
   class Group < Model
     set_search_type
-    define_properties :id, :name, :description, :link, :privacy, :updated_time, :version, :administrator
+    define_properties :id, :name, :description, :link, :privacy,
+      :updated_time, :version, :administrator, :unread, :bookmark_order
 
     hash_populating_accessor :owner, "User", "Page"
     hash_populating_accessor :venue, "Address"
