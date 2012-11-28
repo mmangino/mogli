719e6f1678b47f8f2cf3ded18b562cf0f2ade7b1
Yitao Sun <yitaosun@gmail.com>
2012-11-28 11:01:20 +0800

    Changed Model::hash_populating_accessor to bypass map_data when the hash is unexpectedly un-json.

--- lib/mogli/model.rb
+++ lib/mogli/model.rb
@@ -102,7 +102,7 @@ module Mogli
 
     def self.hash_populating_accessor(method_name,*klass)
       define_method "#{method_name}=" do |hash|
-        instance_variable_set("@#{method_name}",client.map_data(hash,klass))
+        instance_variable_set("@#{method_name}",hash.is_a?(String)?hash:client.map_data(hash,klass))
       end
       define_method "#{method_name}" do
         instance_variable_get "@#{method_name}"
