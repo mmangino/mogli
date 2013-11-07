b23b96fbff5c0b77478793b5a978e1e09d4f648d
Rob Rivas <rrivas@goldstar.com>
2013-11-07 14:44:41 -0800

    Add members association to friendlist

--- lib/mogli/friendlist.rb
+++ lib/mogli/friendlist.rb
@@ -1,10 +1,11 @@
 module Mogli
-  
+
   # requires read_friendlists permission
   # https://developers.facebook.com/docs/reference/api/FriendList/
-  
+
   class FriendList < Model
     define_properties :name, :list_type, :id
-    
+
+    has_association :members, "User"
   end
 end
